e61da9cf30a9936e4b3954cacb2e31db4a40d2c6
Pawan Gurung <pawan.grgn@gmail.com>
2024-09-12 23:56:33 +0545

    new

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lttqzvaxo#ojg(@@02)8qy7uxt5t=d7oce8x$&y_=8hz)!9vam
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
